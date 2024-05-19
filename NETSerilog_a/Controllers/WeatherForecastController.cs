40e91dd78805bbeb6840d362652f1711ce238a71
maksatgw <garyagdyyev@outlook.com>
2024-05-19 11:40:41 +0300

    Add project files.

---
+++ NETSerilog_a/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace NETSerilog_a.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                _logger.LogInformation("Get tetiklenmiþtir.");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToArray();
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{ex.Message}");
+                throw new Exception();
+            }
+            
+        }
+    }
+}
